60fae8687047497e31f38cb4fec9c9b651be9676
salimcherifi <salimcherifi@gmail.com>
2018-10-09 08:09:33 +0200

    nouvelle version de dtwtest

--- src/myDTW.java
+++ src/myDTW.java
@@ -6,7 +6,6 @@ public class myDTW extends DTWHelper {
 	public float DTWDistance(Field unknown, Field known) {
 		// Methode qui calcule le score de la DTW
 		// entre 2 ensembles de MFCC
-//test
 
         int w0 = 1;
         int w1 = 2;
@@ -15,9 +14,12 @@ public class myDTW extends DTWHelper {
         float [][] dtw = new float[unknown.getLength()+1][known.getLength()+1];
         dtw[0][0] = 0.f;
 
-        for(int j = 1; j < unknown.getLength()+1; j++){
+        for(int j = 1; j < known.getLength()+1; j++){
             dtw[0][j] = Float.MAX_VALUE;
+        }
+        for(int j = 1; j < unknown.getLength()+1; j++){
             dtw[j][0] = Float.MAX_VALUE;
+
         }
       
 
--- src/myDTWtest.java
+++ src/myDTWtest.java
@@ -1,4 +1,7 @@
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,63 +14,86 @@ import fr.enseeiht.danck.voice_analyzer.defaults.DTWHelperDefault;
 
 public class myDTWtest {
 
-		protected static final int MFCCLength = 13;
+	//protected static final int MFCCLength = 13;
 
-		public static void main(String[] args) throws IOException, InterruptedException {
-			
-			DTWHelper myDTWHelper= new myDTW();
-			DTWHelper DTWHelperDefault= new DTWHelperDefault();
-			 
-			// Chemin de recherche des fichiers sons
-		    String base = "/test_res/audio/";
-		    
-		    // Appel a l'extracteur par defaut (calcul des MFCC)
-		    Extractor extractor = Extractor.getExtractor();
-		    
-			// Etape 1. Lecture de Alpha
-		    List<String> files = new ArrayList<>();
-		    files.add(base + "Alpha.csv");
-		    WindowMaker windowMaker = new MultipleFileWindowMaker(files);
-		    
-		    // Etape 2. Recuperation des MFCC du mot Alpha
-		    MFCC[] mfccsAlpha = new MFCC[MFCCLength];
-	        for (int i = 0; i < mfccsAlpha.length; i++) {
-	            mfccsAlpha[i] = extractor.nextMFCC(windowMaker);
-	        }
-	        
-	        // Etape 3. Construction du Field (ensemble de MFCC) de alpha
-	        Field alphaField= new Field(mfccsAlpha);
-	        
-	        // Si on veut rajouter de nouveaux mots, il suffit de repeter les etapes 1 a 3
-	        
-	        // Par ex., on peut tester que la distance entre alpha et alpha c'est 0
-	        float mydistanceAlphaAlpha = myDTWHelper.DTWDistance(alphaField, alphaField);
-	        float distanceAlphaAlphadefault= DTWHelperDefault.DTWDistance(alphaField, alphaField);
-	        
-	        System.out.println("myDTW - valeur distance Alpha-Alpha calculee : "+mydistanceAlphaAlpha);
-	        System.out.println("DTWHelperDefault - valeur distance Alpha-Alpha calculee : "+distanceAlphaAlphadefault);
-		
-	        // Calcul de la distance entre Alpha et Bravo
-	        
-	        // Etape 1. Lecture de Bravo
-	        files= new ArrayList<>();
-		    files.add(base + "Bravo.csv");
-		    windowMaker = new MultipleFileWindowMaker(files);
-		    
-		 // Etape 2. Recuperation des MFCC du mot Bravo
-		    MFCC[] mfccsBravo= new MFCC[MFCCLength];
-	        for (int i = 0; i < mfccsBravo.length; i++) {
-	            mfccsBravo[i] = extractor.nextMFCC(windowMaker);
-	        }
-	        
-	        // Etape 3. Construction du Field (ensemble de MFCC) de Bravo
-	        Field bravoField= new Field(mfccsBravo);
+	// Fonction permettant de calculer la taille des Fields
+	//c'est-à-dire le nombre de MFCC du Field
+	static int FieldLength(String fileName) throws IOException {
+		int counter= 0;
+		File file = new File(System.getProperty("user.dir") + fileName);
+		for (String line : Files.readAllLines(file.toPath(), Charset.defaultCharset())) {
+			counter++;
+		}
+		return 2*Math.floorDiv(counter, 512);
+	}
 
-	       float mydistanceAlphaBravo= myDTWHelper.DTWDistance(alphaField, bravoField);
-	        float distanceAlphaBravodefault= DTWHelperDefault.DTWDistance(alphaField, bravoField);
-	        
-	        System.out.println("myDTW - valeur distance Alpha-Bravo calculee : "+mydistanceAlphaBravo);
-	        System.out.println("DTWHelperDefault - valeur distance Alpha-Bravo calculee : "+distanceAlphaBravodefault);
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		int MFCCLength;
+		DTWHelper myDTWHelper= new myDTW();
+		DTWHelper DTWHelperDefault= new DTWHelperDefault();
+
+		// Chemin de recherche des fichiers sons
+		String base = "/test_res/audio/";
+
+		// Appel a l'extracteur par defaut (calcul des MFCC)
+		Extractor extractor = Extractor.getExtractor();
+
+		// Etape 1. Lecture de Alpha
+		List<String> files = new ArrayList<>();
+		files.add(base + "Alpha.csv");
+		WindowMaker windowMaker = new MultipleFileWindowMaker(files);
+
+		// Etape 2. Recuperation des MFCC du mot Alpha
+		MFCCLength= FieldLength(base+"Alpha.csv");
+		MFCC[] mfccsAlpha = new MFCC[MFCCLength];
+
+		for (int i = 0; i < mfccsAlpha.length; i++) {
+			mfccsAlpha[i] = extractor.nextMFCC(windowMaker);
 		}
 
+		// Etape 3. Construction du Field (ensemble de MFCC) de alpha
+		Field alphaField= new Field(mfccsAlpha);
+
+		// Affichage du Field et de chaque MFCC
+//		System.out.println(alphaField.toString());
+//		for (int i =0; i<alphaField.getLength(); i++ )
+//			System.out.println(i+": "+alphaField.getMFCC(i).toString());
+
+		// Si on veut rajouter de nouveaux mots, il suffit de repeter les etapes 1 a 3
+
+		// Par ex., on peut tester que la distance entre alpha et alpha c'est 0
+		 float mydistanceAlphaAlpha= myDTWHelper.DTWDistance(alphaField, alphaField);
+		float distanceAlphaAlphadefault= DTWHelperDefault.DTWDistance(alphaField, alphaField);
+
+		 System.out.println("myDTW - valeur distance Alpha-Alpha calculee : "+mydistanceAlphaAlpha);
+		System.out.println("DTWHelperDefault - valeur distance Alpha-Alpha calculee : "+distanceAlphaAlphadefault);
+
+		// La distance entre Alpha et Bravo
+
+		// Etape 1. Lecture de Bravo
+		files= new ArrayList<>();
+		files.add(base + "Bravo.csv");
+		MFCCLength= FieldLength(base+"Bravo.csv");
+		windowMaker = new MultipleFileWindowMaker(files);
+
+		// Etape 2. Recuperation des MFCC du mot Bravo
+		MFCC[] mfccsBravo= new MFCC[MFCCLength];
+		for (int i = 0; i < mfccsBravo.length; i++) {
+			mfccsBravo[i] = extractor.nextMFCC(windowMaker);
+		}
+
+		// Etape 3. Construction du Field (ensemble de MFCC) de Bravo
+		Field bravoField= new Field(mfccsBravo);
+
+		float mydistanceAlphaBravo= myDTWHelper.DTWDistance(alphaField, bravoField);
+		float distanceAlphaBravodefault= DTWHelperDefault.DTWDistance(alphaField, bravoField);
+
+		System.out.println("myDTW - valeur distance Alpha-Bravo calculee : "+mydistanceAlphaBravo);
+		System.out.println("DTWHelperDefault - valeur distance Alpha-Bravo calculee : "+distanceAlphaBravodefault);
+
+
+	}
 }
+
