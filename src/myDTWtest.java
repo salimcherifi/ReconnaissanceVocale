56260e63dd0a8671fbfab2d7aefbc07cbfcd6a2a
salimcherifi <salimcherifi@gmail.com>
2018-10-17 18:22:16 +0200

    test

--- src/myDTWtest.java
+++ src/myDTWtest.java
@@ -18,18 +18,17 @@ public class myDTWtest {
     public static void main(String[] args) throws IOException, InterruptedException {
         myDTW dtw = new myDTW();
 
-        String nomF1 = "/corpus/dronevolant_nonbruite/F01_avance.csv";
-        String nomF2 = "/myAudio/avance.csv";
-        Field f1 = dtw.getFieldMFCCs(nomF1);
-        Field f2 = dtw.getFieldMFCCs(nomF2);
-        float distance = dtw.calcDistanceField(f1, f2);
+//        String nomF1 = "/corpus/dronevolant_nonbruite/F01_avance.csv";
+//        String nomF2 = "/myAudio/avance.csv";
+//        Field f1 = dtw.getFieldMFCCs(nomF1);
+//        Field f2 = dtw.getFieldMFCCs(nomF2);
+//        float distance = dtw.calcDistanceField(f1, f2);
+//
+//
+//        System.out.println("Distance entre "+nomF1+"      et      "+nomF2+" :        " + distance);
 
+        dtw.matriceConfusion("./corpus/dronevolant_nonbruite","./corpus/dronevolant_nonbruite");
 
-        System.out.println("Distance entre "+nomF1+"      et      "+nomF2+" :        " + distance);
-
-
-        DTWHelper myDTWHelper = new myDTW();
-        //((myDTW) myDTWHelper).getFilesFromFolder("./corpus/");
 
 
     }
