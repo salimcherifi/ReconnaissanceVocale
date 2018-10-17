32ea8f10b9ff62a4538c7552ab5da513db95e6a6
salimcherifi <salimcherifi@gmail.com>
2018-10-17 16:52:55 +0200

    gg

--- src/myDTW.java
+++ src/myDTW.java
@@ -116,22 +116,23 @@ public class myDTW extends DTWHelper {
     }
 
 
-    public void distanceFolders(String folder1, String folder2) throws IOException, InterruptedException {
+    public float distanceFolders(String folder1, String folder2) throws IOException, InterruptedException {
         int folder1Length = new File(folder1).listFiles().length;
         int folder2Length = new File(folder2).listFiles().length;
         String[] files1 = getFilesFromFolder(folder1,"file");
         String[] files2 = getFilesFromFolder(folder2,"file");
-        String min;
+        float min;
         float valMin = Float.MAX_VALUE;
         int k = 0;
         for (int i = 0; i < folder1Length; i++) {
             for (int j = 0; j < folder2Length; j++) {
                 float distance = calcDistanceField(getFieldMFCCs(files1[i]),getFieldMFCCs(files2[j]));
                 if (distance < valMin){
-                    min = files1[i]+":"+files2[j];
+                    valMin = distance;
                 }
             }
         }
+        return valMin;
     }
 
     public Float[][] matriceConfusion(String folderRef, String folderTest) {
